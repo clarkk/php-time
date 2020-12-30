b870a5a88ecff8f9426c93944baca55becb908da
clarkk <clark@electrobeat.dk>
2020-12-30 14:30:49 +0100

    updated

--- Time.php
+++ Time.php
@@ -3,5 +3,17 @@
 namespace Time;
 
 class Time {
+	const DEFAULT_TIMEZONE = 'Europe/Copenhagen';
 	
+	static public function timestamp(int $base_time=0): string{
+		return date('Y-m-d H:i:s', self::time_local_offset($base_time));
+	}
+	
+	static public function file_timestamp(int $base_time=0): string{
+		return date('Y-m-d_His', self::time_local_offset($base_time));
+	}
+	
+	static private function time_local_offset(int $base_time=0, string $timezone=self::DEFAULT_TIMEZONE){
+		return ($base_time ?: time()) + (new \DateTimeZone($timezone))->getOffset(new \DateTime('now'));
+	}
 }
