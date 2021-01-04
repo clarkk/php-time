65eeeb1e51d2d26fff4c12545aa4b1be4990d2a8
clarkk <clark@electrobeat.dk>
2021-01-04 14:03:18 +0100

    added time_today

--- Time.php
+++ Time.php
@@ -5,6 +5,12 @@ namespace Time;
 class Time {
 	const DEFAULT_TIMEZONE = 'Europe/Copenhagen';
 	
+	static public function time_today(): int{
+		$time = self::time_local_offset();
+		
+		return mktime(0,0,0, date('m', $time), date('d', $time), date('Y', $time));
+	}
+	
 	static public function timestamp(int $base_time=0): string{
 		return date('Y-m-d H:i:s', self::time_local_offset($base_time));
 	}
@@ -13,7 +19,7 @@ class Time {
 		return date('Y-m-d-His', self::time_local_offset($base_time));
 	}
 	
-	static private function time_local_offset(int $base_time=0, string $timezone=self::DEFAULT_TIMEZONE){
-		return ($base_time ?: time()) + (new \DateTimeZone($timezone))->getOffset(new \DateTime('now'));
+	static private function time_local_offset(int $base_time=0, string $timezone=self::DEFAULT_TIMEZONE): int{
+		return $base_time ?: time() + (new \DateTimeZone($timezone))->getOffset(new \DateTime('now'));
 	}
 }
