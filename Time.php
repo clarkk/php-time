8b8fae0433d801474d990f20f0a8a7f452d16122
clarkk <clark@electrobeat.dk>
2022-03-13 19:24:27 +0100

    added date method

--- Time.php
+++ Time.php
@@ -3,7 +3,9 @@
 namespace Time;
 
 class Time {
-	const DEFAULT_TIMEZONE = 'Europe/Copenhagen';
+	const DEFAULT_TIMEZONE 	= self::TZ_COPENHAGEN;
+	
+	const TZ_COPENHAGEN 	= 'Europe/Copenhagen';
 	
 	static public function time_today(): int{
 		$time = self::time_local_offset();
@@ -19,7 +21,7 @@ class Time {
 		return date('Y-m-d H:i:s'.substr((string)microtime(), 1, 4), self::time_local_offset());
 	}
 	
-	public static function timestamp_rfc(int $time=0): string{
+	static public function timestamp_rfc(int $time=0): string{
 		return date('D, j M Y H:i:s O', self::time_local_offset($time));
 	}
 	
@@ -33,5 +35,47 @@ class Time {
 	
 	static public function time_local_offset(int $time=0, string $timezone=self::DEFAULT_TIMEZONE): int{
 		return $time ?: time() + (new \DateTimeZone($timezone))->getOffset(new \DateTime('now'));
+	}
+	
+	static public function date(string $date, string $sep='-'): int{
+		if(strpos($date, $sep)){
+			$parts = explode($sep, $date);
+		}
+		else{
+			$parts = [
+				substr($date, 0, 2),
+				substr($date, 2, 2),
+				substr($date, 4, 4)
+			];
+		}
+		
+		$parts[0] ??= 0;
+		$parts[1] ??= 0;
+		$parts[2] ??= 0;
+		
+		if(strlen($parts[0]) == 4){
+			$parts = [
+				'y'	=> $parts[0],
+				'm'	=> $parts[1],
+				'd'	=> $parts[2]
+			];
+		}
+		else{
+			$parts = [
+				'd'	=> $parts[0],
+				'm'	=> $parts[1],
+				'y'	=> $parts[2] ?: date('Y')
+			];
+		}
+		
+		if(strlen($parts['y']) == 2){
+			$parts['y'] = substr(date('Y'), 0, 2).$parts['y'];
+		}
+		
+		if(checkdate($parts['m'], $parts['d'], $parts['y'])){
+			return mktime(0,0,0, $parts['m'], $parts['d'], $parts['y']);
+		}
+		
+		return -1;
 	}
 }
