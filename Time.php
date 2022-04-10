533c6a0041aa5dc07257c3efb22f10300e2dd1fa
clarkk <clark@electrobeat.dk>
2022-04-11 01:32:28 +0200

    added format method and improved timezone

--- Time.php
+++ Time.php
@@ -3,42 +3,81 @@
 namespace Time;
 
 class Time {
-	const DEFAULT_TIMEZONE 	= self::TZ_COPENHAGEN;
+	const DEFAULT_TIMEZONE 	= 'Europe/Copenhagen';
 	
-	const TZ_COPENHAGEN 	= 'Europe/Copenhagen';
+	static private $locale;
+	
+	static public function init(string $locale){
+		self::$locale = $locale;
+	}
+	
+	static public function format(string $format, int $time_utc, bool $apply_timezone=true): string{
+		// https://unicode-org.github.io/icu/userguide/format_parse/datetime/#date-field-symbol-table
+		
+		/*
+			y 		= year
+			yy 		= year short
+			M 		= month numeric
+			MM 		= month numeric zerofill
+			MMM 	= month alpha short
+			MMMM 	= month alpha full
+			d 		= day
+			dd 		= day zerofill
+			H 		= 24-hour
+			HH 		= 24-hour zerofill
+			m 		= minutes
+			mm 		= minutes zerofill
+			s 		= seconds
+			ss 		= seconds zerofill
+			S 		= fractional second
+			SS 		= fractional second 2 decimals
+			SSS 	= fractional second 3 decimals
+			e 		= weekday numeric
+			ee 		= weekday numeric zerofill
+			eee 	= weekday alpha short
+			eeee 	= weekday alpha full
+			w 		= week of year
+			ww 		= week of year zerofill
+		*/
+		
+		return (new \IntlDateFormatter(
+			self::$locale,
+			\IntlDateFormatter::NONE,
+			\IntlDateFormatter::NONE,
+			$apply_timezone ? self::DEFAULT_TIMEZONE : null,
+			null,
+			$format)
+		)->format(self::create_time($time_utc, false));
+	}
 	
 	static public function time_today(): int{
-		$time = self::time_local_offset();
+		$time = time() + self::time_local_offset();
 		
 		return mktime(0,0,0, date('m', $time), date('d', $time), date('Y', $time));
 	}
 	
-	static public function datestamp(int $time=0): string{
-		return date('Y-m-d', self::time_local_offset($time));
+	static public function datestamp(int $time=0, bool $apply_timezone=true): string{
+		return self::create_time($time, $apply_timezone)->format('Y-m-d');
 	}
 	
-	static public function timestamp(int $time=0): string{
-		return date('Y-m-d H:i:s', self::time_local_offset($time));
+	static public function timestamp(int $time=0, bool $apply_timezone=true): string{
+		return self::create_time($time, $apply_timezone)->format('Y-m-d H:i:s');
 	}
 	
 	static public function timestamp_ms(): string{
-		return date('Y-m-d H:i:s'.substr((string)microtime(), 1, 4), self::time_local_offset());
+		return self::create_time()->format('Y-m-d H:i:s').substr((string)microtime(), 1, 4);
 	}
 	
-	static public function timestamp_rfc(int $time=0): string{
-		return date('D, j M Y H:i:s O', self::time_local_offset($time));
+	static public function timestamp_rfc(int $time=0, bool $apply_timezone=true): string{
+		return self::create_time($time, $apply_timezone)->format('D, j M Y H:i:s O');
 	}
 	
-	static public function file_datestamp(int $time=0): string{
-		return date('Y-m-d', self::time_local_offset($time));
+	static public function file_datestamp(int $time=0, bool $apply_timezone=true): string{
+		return self::create_time($time, $apply_timezone)->format('Y-m-d');
 	}
 	
-	static public function file_timestamp(int $time=0): string{
-		return date('Y-m-d-His', self::time_local_offset($time));
-	}
-	
-	static public function time_local_offset(int $time=0, string $timezone=self::DEFAULT_TIMEZONE): int{
-		return $time ?: time() + (new \DateTimeZone($timezone))->getOffset(new \DateTime('now'));
+	static public function file_timestamp(int $time=0, bool $apply_timezone=true): string{
+		return self::create_time($time, $apply_timezone)->format('Y-m-d-His');
 	}
 	
 	static public function time_offset(bool $interval_month, int $interval_value, int $time): int{
@@ -120,5 +159,20 @@ class Time {
 		}
 		
 		return -1;
+	}
+	
+	static private function create_time(int $time_utc=0, bool $apply_timezone=true){
+		$timezone = $apply_timezone ? new \DateTimeZone(self::DEFAULT_TIMEZONE) : null;
+		
+		if($time_utc){
+			return (new \DateTime('', $timezone))->setTimestamp($time_utc);
+		}
+		else{
+			return new \DateTime('now', $timezone);
+		}
+	}
+	
+	static private function time_local_offset(int $time_utc=0): int{
+		return (new \DateTimeZone(self::DEFAULT_TIMEZONE))->getOffset($time_utc ? (new \DateTime)->setTimestamp($time_utc) : new \DateTime('now'));
 	}
 }
