5bf51154a6074c047e30af796a98778082eb7d8d
clarkk <clark@electrobeat.dk>
2022-04-12 13:39:22 +0200

    added time_offset_months and time_offset_days

--- Time.php
+++ Time.php
@@ -2,6 +2,13 @@
 
 namespace Time;
 
+/*
+ *	Default timezone (when retrieving UNIX timestamps, storing in the database and sending to the browser) must at any time by UTC.
+ *	Javascript expects UNIX timestamps to be UTC and will automatically apply user timezone.
+ *	If your application handles time by a different timezone, Javascript will show the user an incorrect time/date in the browser.
+ */
+date_default_timezone_set('UTC');
+
 class Time {
 	public const TZ_COPENHAGEN = 'Europe/Copenhagen';
 	
@@ -58,15 +65,15 @@ class Time {
 		return mktime(0,0,0, date('m', $time), date('d', $time), date('Y', $time));
 	}
 	
-	static public function datestamp(int $time=0, bool $apply_timezone=false): string{
+	static public function datestamp(?int $time=null, bool $apply_timezone=false): string{
 		return self::create_time($time, $apply_timezone)->format('Y-m-d');
 	}
 	
-	static public function timestamp(int $time=0, bool $apply_timezone=false): string{
+	static public function timestamp(?int $time=null, bool $apply_timezone=false): string{
 		return self::create_time($time, $apply_timezone)->format('Y-m-d H:i:s');
 	}
 	
-	static public function timestamp_rfc(int $time=0, bool $apply_timezone=false): string{
+	static public function timestamp_rfc(?int $time=null, bool $apply_timezone=false): string{
 		return self::create_time($time, $apply_timezone)->format('D, j M Y H:i:s O');
 	}
 	
@@ -74,51 +81,84 @@ class Time {
 		return self::create_time()->format('Y-m-d H:i:s').substr((string)microtime(), 1, 4);
 	}
 	
-	static public function file_datestamp(int $time=0): string{
+	static public function file_datestamp(?int $time=null): string{
 		return self::create_time($time)->format('Y-m-d');
 	}
 	
-	static public function file_timestamp(int $time=0): string{
+	static public function file_timestamp(?int $time=null): string{
 		return self::create_time($time)->format('Y-m-d-His');
 	}
 	
-	static public function time_offset(bool $interval_month, int $interval_value, int $time): int{
-		//	months
-		if($interval_month){
-			$day 	= date('d', $time);
-			$month 	= date('n', $time);
-			$year 	= date('Y', $time);
-			
-			for($i=0; $i<$interval_value; $i++){
-				if($month == 12){
-					$month = 1;
-					$year++;
-				}
-				else{
-					$month++;
-				}
-			}
-			
-			if(checkdate($month, $day, $year)){
-				return mktime(0,0,0, $month, $day, $year) - (60*60*24);
+	static public function time_offset_days(int $time, int $days, bool $count_start_day=false): int{
+		if($count_start_day){
+			if($days > 0){
+				$days--;
 			}
 			else{
-				while(!checkdate($month, $day, $year)){
-					$day--;
-				}
-				
-				return mktime(0,0,0, $month, $day, $year);
+				$days++;
+			}
+			
+			if(!$days){
+				return $time;
 			}
 		}
-		//	days
-		else{
-			return $time + (60*60*24* ($interval_value - 1));
+		
+		return $time += 60*60*24 * $days;
+	}
+	
+	static public function time_offset_months(int $time, int $months, bool $count_start_day=false): int{
+		$day 	= date('d', $time);
+		$month 	= date('n', $time);
+		$year 	= date('Y', $time);
+		
+		if($months > 0){
+			$is_increase 	= true;
+			$year 			+= floor($months / 12);
 		}
+		else{
+			$is_increase 	= false;
+			$year 			+= ceil($months / 12);
+		}
+		
+		$months 	= $months % 12;
+		$month 		+= $months;
+		
+		if($month > 12){
+			$year++;
+			$month 	-= 12;
+		}
+		elseif($month < 1){
+			$year--;
+			$month 	+= 12;
+		}
+		
+		if(checkdate($month, $day, $year)){
+			$time_offset = mktime(0,0,0, $month, $day, $year);
+		}
+		else{
+			while(!checkdate($month, $day, $year)){
+				$day--;
+			}
+			
+			$time_offset = mktime(0,0,0, $month, $day, $year);
+		}
+		
+		if($count_start_day){
+			if($is_increase){
+				$time_offset -= 60*60*24;
+			}
+			else{
+				$time_offset += 60*60*24;
+			}
+		}
+		
+		return $time_offset;
 	}
 	
 	static public function date(string $date): int{
 		$date = trim($date);
 		
+		//	Date is pure digits without date separator
 		if(ctype_digit($date)){
 			$parts = [
 				substr($date, 0, 2),
@@ -126,9 +166,11 @@ class Time {
 				substr($date, 4, 4)
 			];
 		}
+		//	Date separator is hyphen (-)
 		elseif(strpos($date, '-')){
 			$parts = explode('-', $date);
 		}
+		//	Date separator is something else
 		else{
 			$parts = preg_split('/\D/', $date);
 		}
@@ -148,14 +190,16 @@ class Time {
 			$parts = [
 				'd'	=> $parts[0],
 				'm'	=> $parts[1],
-				'y'	=> $parts[2] ?: date('Y')
+				'y'	=> $parts[2] ?: date('Y')	// If year is unset apply current year
 			];
 		}
 		
+		//	Apply full year representation (4 digits) if only 2 digits
 		if(strlen($parts['y']) == 2){
 			$parts['y'] = substr(date('Y'), 0, 2).$parts['y'];
 		}
 		
+		//	Check if date is valid and return UNIX time
 		if(checkdate($parts['m'], $parts['d'], $parts['y'])){
 			return mktime(0,0,0, $parts['m'], $parts['d'], $parts['y']);
 		}
@@ -163,9 +207,8 @@ class Time {
 		return -1;
 	}
 	
-	static private function create_time(int $time=0, bool $apply_timezone=true){
+	static private function create_time(?int $time=null, bool $apply_timezone=true){
 		$timezone = null;
-		
 		if($apply_timezone){
 			if(!self::$timezone){
 				throw new Error('No timezone is defined');
@@ -174,15 +217,10 @@ class Time {
 			$timezone = new \DateTimeZone(self::$timezone);
 		}
 		
-		if($time){
-			return (new \DateTime('', $timezone))->setTimestamp($time);
-		}
-		else{
-			return new \DateTime('now', $timezone);
-		}
+		return $time ? (new \DateTime('', $timezone))->setTimestamp($time) : new \DateTime('now', $timezone);
 	}
 	
-	static private function time_local_offset(int $time=0): int{
+	static private function time_local_offset(?int $time=null): int{
 		return (new \DateTimeZone(self::$timezone))->getOffset($time ? (new \DateTime)->setTimestamp($time) : new \DateTime('now'));
 	}
 }
