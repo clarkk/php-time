65d4f1b41a8fa930d613017fc988a1c8ba355617
clarkk <clark@electrobeat.dk>
2022-04-12 15:22:53 +0200

    code reuse

--- Offset.php
+++ Offset.php
@@ -3,6 +3,8 @@
 namespace Time;
 
 class Offset {
+	private const DAY = 60*60*24;
+	
 	static public function time_days(int $time, int $days, bool $count_start_day=false): int{
 		if($count_start_day){
 			if($days > 0){
@@ -17,7 +19,7 @@ class Offset {
 			}
 		}
 		
-		return $time += 60*60*24 * $days;
+		return $time += self::DAY * $days;
 	}
 	
 	static public function time_months(int $time, int $months, bool $count_start_day=false): int{
@@ -47,25 +49,29 @@ class Offset {
 		}
 		
 		if(checkdate($month, $day, $year)){
-			$time_offset = mktime(0,0,0, $month, $day, $year);
+			$time_offset = self::time_date($month, $day, $year);
 		}
 		else{
 			while(!checkdate($month, $day, $year)){
 				$day--;
 			}
 			
-			$time_offset = mktime(0,0,0, $month, $day, $year);
+			$time_offset = self::time_date($month, $day, $year);
 		}
 		
 		if($count_start_day){
 			if($is_increase){
-				$time_offset -= 60*60*24;
+				$time_offset -= self::DAY;
 			}
 			else{
-				$time_offset += 60*60*24;
+				$time_offset += self::DAY;
 			}
 		}
 		
 		return $time_offset;
+	}
+	
+	static private function time_date(int $month, int $day, int $year): int{
+		return mktime(0,0,0, $month, $day, $year);
 	}
 }
